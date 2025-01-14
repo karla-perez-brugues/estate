96f6b9fcc3955d47278089c7aed588dfc6edcd3a
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-01-14 16:41:49 +0100

    add user model controller and repository

---
+++ backend/src/main/java/com/estate/model/User.java
@@ -0,0 +1,68 @@
+package com.estate.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "password")
+	private String password;
+	
+	public User() {}
+	
+	public User(String email, String name, String password) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
