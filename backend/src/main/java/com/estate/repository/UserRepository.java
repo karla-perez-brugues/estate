7c15117f23e7e6a7149b8fd4ad95992dc160b5a1
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-01-14 16:41:49 +0100

    add user model controller and repository

---
+++ backend/src/main/java/com/estate/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.estate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.estate.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
