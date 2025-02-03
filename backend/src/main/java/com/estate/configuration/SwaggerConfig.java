fb92519b19cccd2c8f000c61816f321b8320b6a5
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-02-03 15:23:11 +0100

    add swagger config to use bearer token

---
+++ backend/src/main/java/com/estate/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.estate.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Authentication"))
+            .components(new Components()
+                .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+            .info(new Info()
+                .title("Estate API")
+                .version("1.0")
+                .contact(new Contact()
+                    .name("Karla PEREZ")
+                    .email( "karla.perezbrugues@gmail.com")
+                    .url("https://github.com/karla-perez-brugues/estate")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer");
+    }
+
+}
