61f8424c9eb90f37a2ef4d318169e9d871478a88
Karla PEREZ <karla.perezbrugues@gmail.com>
2025-02-05 17:07:26 +0100

    mark owner as non nullable

--- backend/src/main/java/com/estate/model/Rental.java
+++ backend/src/main/java/com/estate/model/Rental.java
@@ -1,12 +1,6 @@
 package com.estate.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.util.Date;
 
@@ -34,6 +28,7 @@ public class Rental {
 	private String description;
 	
 	@ManyToOne
+	@JoinColumn(nullable = false)
 	private User owner;
 
 	@Column(name = "created_at")
